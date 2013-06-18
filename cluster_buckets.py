d8024a01313d4ba7ebf08c5435b20e0ce1d4926d
Steve McMaster <mcmaster@hurricanelabs.com>
2013-06-18 15:34:45 -0400

    Add cluster_buckets.py helper script for displaying invalid buckets

---
+++ cluster_buckets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from splunk import SplunkServer
+
+import optparse
+
+
+op = optparse.OptionParser()
+op.add_option("-H", dest="hostname", type="string", help="IP or FQDN of the Splunk server")
+op.add_option("-U", dest="username", type="string", help="Username used to log into Splunk")
+op.add_option("-P", dest="password", type="string", help="Password used to log into Splunk")
+op.add_option("-p", dest="port", type="int", default=8089, help="splunkd Port on server, default 8089")
+op.add_option("-n", dest="use_ssl", action="store_false", default=True, help="Disable HTTPS (use http)")
+(options, args) = op.parse_args()
+
+splunkd = SplunkServer(options.hostname, options.username, options.password, options.port, options.use_ssl)
+
+config = splunkd.cluster_config
+
+invalid = list()
+for bucket in splunkd.cluster_buckets:
+    if bucket["standalone"] == "1":
+        continue
+    valid = 0
+    complete = 0
+    for (peer,info) in bucket["peers"].items():
+        if info["search_state"] == "Searchable":
+            valid += 1
+        if info["status"] == "Complete":
+            complete += 1
+    if valid < int(config["search_factor"]):
+        print "Bucket {} is invalid (does not meet search factor of {}".format(bucket["title"], config["search_factor"])
+    if complete < int(config["replication_factor"]):
+        print "Bucket {} is incomplete (does not meet replication factor of {}".format(bucket["title"], config["replication_factor"])
